8cdd0b19d8a3ced0aa6a73586ae37b0d64822022
Astha <asthajain80@gmail.com>
2022-03-16 19:29:13 +0530

    changes

--- RestAssuredTesting/src/test/java/RestTesting/E_Commerce.java
+++ RestAssuredTesting/src/test/java/RestTesting/E_Commerce.java
@@ -1,7 +1,5 @@
 package RestTesting;
 
-import static io.restassured.RestAssured.given;
-
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -9,70 +7,73 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
 public class E_Commerce {
-
 	public static String baseurl = "https://ecommerceservice.herokuapp.com";
 	public static String message;
 	public static String accessToken;
-	@Test(priority = 0)
-	public void signup()
-	{
-		RestAssured.baseURI =baseurl;
+		@Test(priority = 0)
+		public void signup()
+		{
+			RestAssured.baseURI =baseurl;
+			
+		String 	requestbody = "{\r\n"
+				+ "	\"email\": \"jjj1lllaaainastha13@gmail.com\",\r\n"
+				+ "	\"password\": \"krishna@123\"\r\n"
+				+ "}";
+
 		
-	String 	requestbody = "{\n"
-			+ "	\"email\": \"abhishek24442@gmail.com\",\n"
-			+ "	\"password\": \"krishna@123\"\n"
-			+ "}";
-	
-	Response resposne = given()
-			.header("Content-Type","application/json")
-			.body(requestbody)
-			
-			.when()
-			.post("/user/signup")
-			
-			.then()
-			.assertThat().statusCode(201).contentType(ContentType.JSON)
-			.extract().response();	
-	
-	String jsonresponse = resposne.asString();
-	//i want to convert the response in to json format
-	//why do i use jsonpath to convert the string in to a json format
-	JsonPath js = new JsonPath(jsonresponse);
-	//nw i have to fetch the id
-	message = js.get("message");
-	System.out.println(message);
-	
-	
+		Response resposne = given()
+				.header("Content-Type","application/json")
+				.body(requestbody)
+				
+				.when()
+				.post("/user/signup")
+				
+				.then()
+				.assertThat().statusCode(201).contentType(ContentType.JSON)
+				.extract().response();	
+		
+		String jsonresponse = resposne.asString();
+		//i want to convert the response in to json format
+		//why do i use jsonpath to convert the string in to a json format
+		JsonPath js = new JsonPath(jsonresponse);
+		//nw i have to fetch the id
+		message = js.get("message");
+		System.out.println(message);
+		
+		
 	}
-	@Test(priority = 1)
-	public void Login()
-	{
-		RestAssured.baseURI =baseurl;
 		
-	String 	requestbody = "{\n"
-			+ "	\"email\": \"abhishek24442@gmail.com\",\n"
-			+ "	\"password\": \"krishna@123\"\n"
-			+ "}";
-	
-	Response resposne = given()
-			.header("Content-Type","application/json")
-			.body(requestbody)
+		
+		@Test(priority = 1)
+		public void Login()
+		{
+			RestAssured.baseURI =baseurl;
 			
-			.when()
-			.post("/user/login")
-			
-			.then()
-			.assertThat().statusCode(200).contentType(ContentType.JSON)
-			.extract().response();	
-	
-	String jsonresponse = resposne.asString();
-	//i want to convert the response in to json format
-	//why do i use jsonpath to convert the string in to a json format
-	JsonPath js = new JsonPath(jsonresponse);
-	//nw i have to fetch the id
-	accessToken = js.get("accessToken");
-	System.out.println(accessToken);
+		String 	requestbody = "{\r\n"
+				+ "	\"email\": \"jjj1lllaaainastha13@gmail.com\",\r\n"
+				+ "	\"password\": \"krishna@123\"\r\n"
+				+ "}";
+		
+		Response resposne = given()
+				.header("Content-Type","application/json")
+				.body(requestbody)
+				
+				.when()
+				.post("/user/login")
+				
+				.then()
+				.assertThat().statusCode(200).contentType(ContentType.JSON)
+				.extract().response();	
+		
+		String jsonresponse = resposne.asString();
+		//i want to convert the response in to json format
+		//why do i use jsonpath to convert the string in to a json format
+		JsonPath js = new JsonPath(jsonresponse);
+		//nw i have to fetch the id
+		accessToken = js.get("accessToken");
+		System.out.println(accessToken);
 
 }
-}
+	}
--- RestAssuredTesting/src/test/java/RestTesting/Trello.java
+++ RestAssuredTesting/src/test/java/RestTesting/Trello.java
@@ -1,33 +1,31 @@
 package RestTesting;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
 public class Trello {
-
-	public static String baseurl = "https://api.trello.com";
+	public static String baseurl="https://api.trello.com";
 	public static String id;
 	
-	@Test(priority = 0)
+	//@test is testng annotation
+	//void createboard is method
+	@Test(priority=0)
+	
 	public void createBoard()
 	{
-		
-		//i want to fetch the base url so that i can add other thing to that base url
-		//This is the first step to provide base url to the request, Storing url\
-		//restassured is class for given,when,then
-		RestAssured.baseURI = baseurl;
-		
-		//in rest assured we have three keyword
-		//given : pre-condition includes like parameter,header, authorization,
-		//when : condition i am testing like resource //iwe have to provide the name //that is called resource /1/board/ and the method name
-		//then : post condition : is something like response check the response
-		Response resposne = given().queryParam("name", "Ajain Moolya board")
-		.queryParam("key", "b6154828142430bb0cb7fafa33901300")
-		.queryParam("token", "fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
+		//restassured is an library used in pom file
+		//restassured is a class which contains given when then.
+		//baseuri is a method which contains objects
+		RestAssured.baseURI=baseurl;
+		//restassured has three keywords given when and then
+		Response response=given().queryParam("name","Astha jain45 Moolya")
+		.queryParam("key","b6154828142430bb0cb7fafa33901300")
+		.queryParam("token","fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
 		.header("Content-Type","application/json")
 		
 		.when()
@@ -35,29 +33,23 @@ public class Trello {
 		
 		.then()
 		.assertThat().statusCode(200).contentType(ContentType.JSON)
-		.extract().response();	
-		
-		//This is to fetch the response as a string
-		String jsonresponse = resposne.asString();
-		
-		//use JsonPath to convert the string in to a json format
-		JsonPath js = new JsonPath(jsonresponse);
-		
-		//to fetch the id
-		id = js.get("id");
-		
-	}
+		.extract().response();
+		//store respons in string format
+		String jsonresponse=response.asString();
+		System.out.println(jsonresponse);
+		//want to convert response in json format
+		JsonPath js=new JsonPath(jsonresponse);
+		id =js.get("id");
+		System.out.println(id);
+		}
 	
-	//if we make any @Test method enabled =false/ that method will not execute
-	
-	@Test(priority = 1)
+	@Test(priority=1)
 	public void getBoard()
+	
 	{
-		RestAssured.baseURI = baseurl;
-		
-		Response response =	given()
-				.queryParam("key", "b6154828142430bb0cb7fafa33901300")
-				.queryParam("token", "fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
+		RestAssured.baseURI=baseurl;
+		Response response=given().queryParam("key","b6154828142430bb0cb7fafa33901300")
+		.queryParam("token","fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
 		.header("Content-Type","application/json")
 		
 		.when()
@@ -65,30 +57,31 @@ public class Trello {
 		
 		.then()
 		.assertThat().statusCode(200).contentType(ContentType.JSON)
-		.extract().response();	
-	
-		String jsonresponse = response.asString();
+		.extract().response();
+		String jsonresponse=response.asString();
+		
 		System.out.println(jsonresponse);
-	}
+		}
 	
-	@Test(priority = 2)
-	public void DeleteBoard()
-	{
-		RestAssured.baseURI = baseurl;
-		
-		Response response =	given()
-				.queryParam("key", "b6154828142430bb0cb7fafa33901300")
-				.queryParam("token", "fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
-		.header("Content-Type","application/json")
-		
-		.when()
-		.delete("/1/boards/"+id)
-		
-		.then()
-		.assertThat().statusCode(200).contentType(ContentType.JSON)
-		.extract().response();	
 	
-		String jsonresponse = response.asString();
-		System.out.println(jsonresponse);
+		 @Test(priority=2)
+		 public void deleteBoard()
+		 {
+			 RestAssured.baseURI=baseurl;
+		Response response=given().queryParam("key","b6154828142430bb0cb7fafa33901300")
+		 .queryParam("token","fce6c768056faf19be8654d4d83becdc7e9075e94a99ea8e4918af61b36ef7cc")
+		  .header("Content-Type","application/json")
+		  
+		  .when() 
+		  .delete("/1/boards/"+id)
+		  
+		  .then()
+		  .assertThat().statusCode(200).contentType(ContentType.JSON)
+		  .extract().response(); 
+		 String jsonresponse=response.asString();
+		 System.out.println(jsonresponse); 
+		 }
+		
+	
 	}
-}
+
